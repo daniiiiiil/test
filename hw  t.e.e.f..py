520975cabd7da1cadef8edcd49ccb90c7efb9faf
daniiiiiil <93081867+daniiiiiil@users.noreply.github.com>
2022-11-12 20:08:07 +0300

    Add files via upload

---
+++ hw  t.e.e.f..py
@@ -0,0 +1,12 @@
+try:
+    f = int(input('Введите ЧИСЛО!!!'))
+    a = int(input('Второе ЧИСЛО!!!'))
+    print(f / a)
+except ValueError:
+        print('Сказал же ЧИСЛО!!!')
+except ZeroDivisionError:
+        print('IQ как у хлебуска?')
+else:
+        print('Всё хорошо.')
+finally:
+        print('Всё.')
