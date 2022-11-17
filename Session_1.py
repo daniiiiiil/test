d82b37591023c99b8caa2c786f1822f69e38639e
daniiiiiil <93081867+daniiiiiil@users.noreply.github.com>
2022-11-17 17:38:19 +0300

    Update Session_1.py

--- Session_1.py
+++ Session_1.py
@@ -31,3 +31,56 @@ def task_3(l1, l2):
 
 
 task_3(["red", "orange", "green", "blue", "white"], ["black", "yellow", "green", "blue"])
+
+#task 4
+
+def task_4(L):
+    x = int("".join(map(str, L))) #join убирает пробелы и запятые, а map преобразует числа в строки.
+    print(x)
+
+task_4([11, 44, 33])
+
+#task 5
+
+def task_5(num):
+    print(max(num, key=sum))
+
+task_5([[1,2], [3], [0, 12, 17, 6]])
+
+#task 6
+
+def task_6(n):
+    r = 0
+    if n > 0:
+        while n > 0:
+            digit = n % 10
+            n //= 10
+            r = r * 10 + digit
+        print(r)
+    if n < 0:
+        n = str(n)
+        c=len(n) # меряем строку
+        v=n[c::-1] # срез
+        print (v)  
+task_6(2550)
+
+
+# task 8
+
+import math
+def task_8(l):
+    x = []
+    q = 0
+    for i in l:
+        e = l[q]
+        l.pop(q)
+        c = math.prod(l)
+        l.insert(0, e)
+        x.append(c)
+        
+        q+=1
+    print(x)
+    return x
+    
+task_8([1, 2, 3, 4, 5])
+
